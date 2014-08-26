ec7f73656a48dcdc0644ce6337a824c5ffea7be9
Sébastien Viande <sviande@gmail.com>
2014-08-26 11:29:12 +0200

    Git : gitlog move to .gitconfig as git lg.

--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -3,5 +3,3 @@ alias dmesg="dmesg -L"
 alias pong="ping free.fr"
 
 alias c="clear"
-
-alias gitlog='git log --graph --full-history --all --no-prefix --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%x1b[0m%x20%s%x1b[36m%x20%an%x1b[0m%x20"'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,6 @@
 alias dmesg='dmesg -L'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
-alias gitlog='git log --graph --full-history --all --no-prefix --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%x1b[0m%x20%s%x1b[36m%x20%an%x1b[0m%x20"'
 alias grep='grep --color=auto'
 
 alias c='clear'
