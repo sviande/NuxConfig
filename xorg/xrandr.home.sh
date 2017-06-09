d0b51137bf11aba3e76ae30f309298b6e94f8740
Sébastien Viande <sviande@gmail.com>
2017-06-09 18:05:28 +0200

    [Xorg|polybar|i3] Update screen name

--- xorg/xrandr.home.sh
+++ xorg/xrandr.home.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --primary\
-  --output VIRTUAL1 --off \
-  --output eDP1 --mode 1920x1080 --pos 904x1080 --rotate normal \
-  --output VGA1 --mode 1920x1080 --pos 1920x0 --rotate normal \
+xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --primary\
+  --output eDP-1 --mode 1920x1080 --pos 904x1080 --rotate normal \
+  --output VGA-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
