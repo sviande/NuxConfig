9f24c606b84481194797702330ff5f6fbb44c2cc
Sébastien Viande <sviande@gmail.com>
2017-10-18 16:48:55 +0200

    Install: Vim install last lien cause of fzf  zshrc creation

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ ln -s ~/.config/git/.gitconfig ~/.gitconfig
 ln -s ~/.config/vim/.vimrc ~/.vimrc
 ln -s ~/.config/vim/plugin ~/.vim/plugin
 
-vim -E -s -c "source ~/.vimrc" -c PlugInstall -c qa
-mkdir -p ~/tmp/vim-undo
-
 ln -s ~/.config/zsh/.zshrc ~/.zshrc
 ln -s ~/.config/zsh/.zprofile ~/.zprofile
 ln -s ~/.config/zsh/.zshenv ~/.zshenv
+
+vim -E -s -c "source ~/.vimrc" -c PlugInstall -c qa
+mkdir -p ~/tmp/vim-undo
