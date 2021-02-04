4557df56c4ff60998ea2c932b98198c270cbc8f8
Sébastien Viande <sviande@gmail.com>
2021-02-04 11:26:17 +0100

    Fish: fix sway config

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -1,4 +1,6 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
-  exec sway
+  eval (keychain --eval --quiet --noask --agents gpg,ssh id_rsa ~/.ssh/capssionCampaign.pem)
+  exec sway &> /tmp/sway_output
+  exit 0
 end
