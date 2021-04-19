adb9560ef0e42e84b3e219aca74feb7e06d1286b
Sébastien Viande <sviande@gmail.com>
2021-04-19 22:19:21 +0200

    Fish: binding fix

--- fish/conf.d/10-binding.fish
+++ fish/conf.d/10-binding.fish
@@ -1,6 +1,5 @@
-fish_vi_key_bindings
-
 function fish_user_key_bindings
+    fish_vi_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
         bind -M $mode \cr '__fzf_search_history'
