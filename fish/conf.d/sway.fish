82c2fbaad37affb978e8f34586f1a8333e04d7c9
Sébastien Viande <sviande@gmail.com>
2021-06-25 16:14:27 +0200

    Fish: sway start verbose + comment for starting with nvidia

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -1,6 +1,7 @@
 set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   eval (keychain --eval --quiet --noask --agents gpg,ssh id_rsa ~/.ssh/capssionCampaign.pem)
-  exec sway &> /tmp/sway_output
+  exec sway -V &> /tmp/sway_output
+#  exec sway -V --my-next-gpu-wont-be-nvidia &> /tmp/sway_output
   exit 0
 end
