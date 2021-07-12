ee6a10a2bee0549bb4d448fa97745905464132c9
Sébastien Viande <sviande@gmail.com>
2021-07-12 17:29:22 +0200

    Nvim: use lua fo plugins + lsp

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,17 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+use 'wbthomason/packer.nvim'
+use 'tpope/vim-fugitive'
+use 'junegunn/fzf'
+use 'junegunn/fzf.vim'
+use 'w0rp/ale'
+use {
+  'fatih/vim-go',
+  cmd = ':GoUpdateBinaries',
+}
+use 'tpope/vim-surround'
+use 'chriskempson/base16-vim'
+use 'neovim/nvim-lspconfig'
+
+end)
