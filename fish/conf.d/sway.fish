b975b1e49989d1b1a556e1cc7d8816076400b8e5
Sébastien Viande <sviande@gmail.com>
2022-01-04 09:31:03 +0100

    Sway: update nvidia flag

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -2,6 +2,6 @@ set TTY1 (tty)
 if test -z "$DISPLAY"; and test $TTY1 = "/dev/tty1"
   eval (keychain --eval --quiet --noask --agents gpg,ssh id_rsa ~/.ssh/capssionCampaign.pem)
   exec sway -V &> /tmp/sway_output
-#  exec sway -V --my-next-gpu-wont-be-nvidia &> /tmp/sway_output
+  exec sway -V --unsupported-gpu &> /tmp/sway_output
   exit 0
 end
