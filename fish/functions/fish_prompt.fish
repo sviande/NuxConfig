199f22a4947928b1f1e59f76ea68b4a49d4673de
Sébastien Viande <sviande@gmail.com>
2022-08-11 10:42:22 +0200

    Fisher update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
-function fish_prompt -d Hydro
+function fish_prompt --description Hydro
     echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal"
     echo -e "$_hydro_prompt❱$hydro_color_normal "
 end
